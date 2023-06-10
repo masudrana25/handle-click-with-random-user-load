a4f1395c20a69fd73706eec4ffbaea2ef67c584d
masudrana25 <mdmasudrana2512000@gmail.com>
2023-06-10 11:08:17 +0600

    complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,17 +4,30 @@ import User from '../User/User';
 
 const Home = () => {
   const [user, setUsers] = useState([]);
-
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=12')
       .then(res => res.json())
       .then(data => setUsers(data.results))
-  },[]);
+  }, []);
+  const [team, setTeam] = useState([]);
+  // const parent = document.getElementById('showMember');
+  const AddMe = (fullName) => {
+    setTeam([...team, fullName]);
+    // const list = document.createElement('ul');
+    // list.innerHTML = '<li>' + fullName + '</li>';
+    // parent.appendChild(list);
+    // console.log(fullName);
+  };
   return (
     <div>
-      <h1>home</h1>
+      <h1>Handle Click on Different Components</h1>
       {
-        user.map( pd => <User user={pd}></User>)
+        team?.map(pd => <li> {pd}</li>)
+      }
+      {/* <div id="showMember">
+      </div> */}
+      {
+        user.map( pd => <User user={pd} AddMe={AddMe}></User>)
       }
     </div>
   );
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const User = (props) => {
-  const {name,email,picture,phone} = props.user;
+  const { name, email, picture, phone } = props.user;
+  const fullName = name.title + ' ' + name.first + ' ' + name.last;
+  const  AddMe = props.AddMe;
+  const [number, setNumber] = useState('');
+  const ShowNumber = () => { setNumber(phone)}
 
   return (
     <div style={{ border: '2px solid purple', borderRadius: '15px', padding: '15px', margin: '10px', display:'flex',width:'40%'}}>
@@ -9,10 +13,11 @@ const User = (props) => {
         <img src={picture.large} alt="" />
       </div>
       <div style={{padding:'15px'}}>
-        <h1> {name.title +' '+ name.first +' '+ name.last}</h1>
+        <h1>Name: {fullName}</h1>
         <h3>Email : {email}</h3>
-        <h5>Phone : {phone}</h5>
-        <button>Add me</button>
+        <h5>Phone : {number}</h5>
+        <button onClick={ShowNumber}> Show Phone Number</button>
+        <button onClick={() => AddMe(fullName)}>Add me</button>
       </div>
     </div>
   );
